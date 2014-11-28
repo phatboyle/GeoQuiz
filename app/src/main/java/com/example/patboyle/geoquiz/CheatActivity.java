0a1c892b3c334262c05754ad39309674f7d17e9d
Pat Boyle <patboyle@amazon.com>
2014-11-28 09:05:05 -0800

    initial commit

---
+++ app/src/main/java/com/example/patboyle/geoquiz/CheatActivity.java
@@ -0,0 +1,18 @@
+package com.example.patboyle.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by patboyle on 11/26/14.
+ */
+public class CheatActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+
+}
